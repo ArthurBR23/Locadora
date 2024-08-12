727300ab7832a3c7a714cf88f49a2eb68837e0d0
202210417 <202210417@TPCP04LAB1019>
2024-08-12 20:29:53 -0300

    Adicionar arquivos de projeto.

---
+++ Apoio/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using Locacoes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locacoes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public static List<Cliente> Clientes = new List<Cliente>()
+        {
+            new Cliente()
+            {
+                Id = 1,
+                Nome = "Arthur",
+                Email = "tutuzinho@gmail.com",
+                Telefone = "(24) 999873579",
+                Cidade = "Volta Redonda"
+            },
+            new Cliente()
+            {
+                Id = 2,
+                Nome = "Larissossauro",
+                Email = "odeiaKPOP@gmail.com",
+                Telefone = "(24)40028922",
+                Cidade = "Barra Mansa"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(Clientes);
+        }
+    }
+}
---
+++ Apoio/Controllers/FabricanteController.cs
@@ -0,0 +1,29 @@
+﻿using Locacoes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locacoes.Controllers
+{
+    public class FabricanteController : Controller
+    {
+
+        public static List<Fabricante> Fabricantes = new List<Fabricante>()
+        {
+            new Fabricante()
+            {
+                Id = 1,
+                Nome = "Xupinto",
+                Pais = "Cazaquistão"
+            },
+            new Fabricante()
+            {
+                Id = 2,
+                Nome = "Mairão 9 gramas",
+                Pais = "Congo"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(Fabricantes);
+        }
+    }
+}
